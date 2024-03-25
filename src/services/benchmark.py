096617ffa74e2bd1a52b9e196e0185de5747c3ab
Cristian Sandu <cristian.sandu@tum.de>
2024-03-25 22:20:56 +0000

    added CLHT

---
+++ src/services/benchmark.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+    Designed to execute the benchmarks / workflows
+"""
+
+class Benchmark(object):
+    def __init__(self, structure: str = None):
+        self.__structure = structure
